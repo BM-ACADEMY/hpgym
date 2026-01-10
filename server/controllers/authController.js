765d84945fbeaf1b0393310a8508c2d771a57d48
BM-ACADEMY <teamkamarweb@gmail.com>
2026-01-10 11:26:33 +0530

    updated

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -63,7 +63,7 @@ const registerUser = async (req, res) => {
       res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000,
       });
 
@@ -103,7 +103,7 @@ const loginUser = async (req, res) => {
       res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV !== "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000,
       });
 
